47062bc0b4c54329741ebb41ac8e09459fe76d36
Vadim Levchenko <vadiml@wizardsdev.com>
2023-11-23 23:21:53 +0200

    Adjust logs data

--- main.py
+++ main.py
@@ -17,15 +17,18 @@ download_tool_site_tt = config.DOWNLOAD_TOOL_SITE_TT
 download_slide_tool_site = config.DOWNLOAD_SLIDE_TOOL_SITE
 download_slide_video_tool_site = config.DOWNLOAD_SLIDE_VIDEO_TOOL_SITE
 
+
 def iri_to_uri(iri):
     parts = urlsplit(iri)
-    uri = urlunsplit((
-        parts.scheme,
-        parts.netloc.encode('idna').decode('ascii'),
-        quote(parts.path),
-        quote(parts.query, '='),
-        quote(parts.fragment),
-    ))
+    uri = urlunsplit(
+        (
+            parts.scheme,
+            parts.netloc.encode("idna").decode("ascii"),
+            quote(parts.path),
+            quote(parts.query, "="),
+            quote(parts.fragment),
+        )
+    )
     return uri
 
 
@@ -34,11 +37,25 @@ def get_current_time():
     return now.strftime("%d/%m/%Y %H:%M:%S")
 
 
-@bot.message_handler(content_types=['text'])
+def get_chat_identity(message):
+    chat_identity = "Chat ID: " + str(message.chat.id) + "\n"
+    if message.chat.title:
+        chat_identity = chat_identity + "Chat title: " + message.chat.title + "\n"
+    if message.from_user.username:
+        chat_identity = chat_identity + "Username: " + message.from_user.username + "\n"
+    if message.from_user.full_name:
+        chat_identity = (
+            chat_identity + "Full name: " + message.from_user.full_name + "\n"
+        )
+
+    return chat_identity
+
+
+@bot.message_handler(content_types=["text"])
 def get__content(message):
     if "tiktok.com/" in message.text:
-        chat_identity = message.chat.title if message.chat.id < 0 else str(
-            message.chat.id)
+        chat_identity = get_chat_identity(message)
+
         try:
             file = open("id.txt", "r")
         except Exception as e:
@@ -46,102 +63,160 @@ def get__content(message):
             file.close()
             file = open("id.txt", "r")
         s = file.read()
-        id = int(s) if s != '' else 1
+        id = int(s) if s != "" else 1
         file.close()
         file = open("id.txt", "w")
         file.write(str(id + 1))
-        file.close()      
-        print('-------------------------' + '\n' + get_current_time() + " id: " + str(
-            id) + " Chat identity: " + chat_identity)
+        file.close()
+        print(
+            "-------------------------"
+            + "\n"
+            + get_current_time()
+            + " id: "
+            + str(id)
+            + " Chat identity: "
+            + chat_identity
+        )
         url_message = message.text
         start_url = url_message.find("https")
         url = iri_to_uri(url_message[start_url:])
         print(get_current_time() + " id: " + str(id) + " URL: " + url)
 
         data = urllib.parse.urlencode(
-            {'id': url, 'locale': 'en', 'tt': download_tool_site_tt})
-        data = data.encode('ascii')
+            {"id": url, "locale": "en", "tt": download_tool_site_tt}
+        )
+        data = data.encode("ascii")
         req = urllib.request.Request(
             download_tool_site,
             data=data,
             headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-            }
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+            },
         )
         try:
             retry_count = 1
-            while (retry_count <= 10):
-                    try:
-                        time.sleep(0.1)
-                        url_response = urllib.request.urlopen(req)
-                        break
-                    except Exception as e:
-                        bot.send_message(dev_chat_id,
-                                        "Chat identity: " + chat_identity + '\n' + "Retry - " + str(retry_count))
-                        retry_count += 1
+            while retry_count <= 10:
+                try:
+                    time.sleep(0.1)
+                    url_response = urllib.request.urlopen(req)
+                    break
+                except Exception as e:
+                    bot.send_message(
+                        dev_chat_id,
+                        "Chat identity: "
+                        + chat_identity
+                        + "\n"
+                        + "Retry - "
+                        + str(retry_count),
+                    )
+                    retry_count += 1
 
-            response_data = url_response.read().decode('utf-8')
-            soup = BeautifulSoup(response_data, 'html.parser')
-            video_link = soup.select('a[class*="pure-button pure-button-primary is-center u-bl dl-button download_link without_watermark vignette_active"]')
+            response_data = url_response.read().decode("utf-8")
+            soup = BeautifulSoup(response_data, "html.parser")
+            video_link = soup.select(
+                'a[class*="pure-button pure-button-primary is-center u-bl dl-button download_link without_watermark vignette_active"]'
+            )
             if video_link:
-                print(video_link[0]['href'])
-                urllib.request.urlretrieve(video_link[0]['href'], str(id) + ".mp4")
+                print(video_link[0]["href"])
+                urllib.request.urlretrieve(video_link[0]["href"], str(id) + ".mp4")
             else:
                 slides_data_element = soup.select('input[name="slides_data"]')
-                slides_data_value = slides_data_element[0]['value']
-                data = urllib.parse.urlencode(
-                    {'slides_data': slides_data_value})
-                data = data.encode('ascii')
+                slides_data_value = slides_data_element[0]["value"]
+                data = urllib.parse.urlencode({"slides_data": slides_data_value})
+                data = data.encode("ascii")
                 req = urllib.request.Request(
                     download_slide_tool_site,
                     data=data,
                     headers={
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-                    }
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+                    },
                 )
                 slider_url_responce = urllib.request.urlopen(req)
-                hx_redirect = slider_url_responce.getheader('Hx-Redirect')
-                x_orign = slider_url_responce.getheader('X-Origin')
-                slider_video_id = hx_redirect[hx_redirect.rfind('/') + 1 :]
-                slider_video_link = 'https://' + x_orign + download_slide_video_tool_site + str(slider_video_id)
-                print(get_current_time() + " id: " + str(id) + ' Slider video link: ' + slider_video_link)
+                hx_redirect = slider_url_responce.getheader("Hx-Redirect")
+                x_orign = slider_url_responce.getheader("X-Origin")
+                slider_video_id = hx_redirect[hx_redirect.rfind("/") + 1 :]
+                slider_video_link = (
+                    "https://"
+                    + x_orign
+                    + download_slide_video_tool_site
+                    + str(slider_video_id)
+                )
+                print(
+                    get_current_time()
+                    + " id: "
+                    + str(id)
+                    + " Slider video link: "
+                    + slider_video_link
+                )
                 urllib.request.urlretrieve(slider_video_link, str(id) + ".mp4")
 
-
-                    
             retry_count = 1
-            while (retry_count <= 10):
-                    try:
-                        video = open(str(id) + '.mp4', 'rb')
-                        if retry_count != 1:
-                            time.sleep(0.5)
-                        bot.send_media_group(message.chat.id, [InputMediaVideo(video, None, None)],
-                            None, message.id)
-                        break
-                    except Exception as e:
-                        bot.send_message(dev_chat_id,
-                                        "Chat identity: " + chat_identity + '\n' + "Retry send message - " + str(retry_count))
-                        retry_count += 1
-                        video.close()
-                        if retry_count == 11:
-                            raise e
-            
+            while retry_count <= 10:
+                try:
+                    video = open(str(id) + ".mp4", "rb")
+                    if retry_count != 1:
+                        time.sleep(0.5)
+                    bot.send_media_group(
+                        message.chat.id,
+                        [InputMediaVideo(video, None, None)],
+                        None,
+                        message.id,
+                    )
+                    break
+                except Exception as e:
+                    bot.send_message(
+                        dev_chat_id,
+                        "Chat identity: "
+                        + chat_identity
+                        + "\n"
+                        + "Retry send message - "
+                        + str(retry_count),
+                    )
+                    retry_count += 1
+                    video.close()
+                    if retry_count == 11:
+                        raise e
+
             video.close()
             os.remove(str(id) + ".mp4")
-            print(get_current_time() + " id: " +
-                str(id) + ' Success')
+            print(get_current_time() + " id: " + str(id) + " Success")
 
         except Exception as e:
-
-            bot.send_message(dev_chat_id, "Chat identity: " +
-                            chat_identity + '\n' + 'Error: ' + str(e )+ '\n' + " URL: " + url )
+            bot.send_message(
+                dev_chat_id,
+                "Chat identity: "
+                + chat_identity
+                + "\n"
+                + "Error: "
+                + str(e)
+                + "\n"
+                + "URL: "
+                + url,
+            )
             file = open("logs_errors.txt", "a")
-            file.write(get_current_time() + " id: " + str(id) + '\n' + str(message.chat.id) + '\n' + url + '\n' + str(
-                e) + '\n' + '\n')
+            file.write(
+                get_current_time()
+                + " id: "
+                + str(id)
+                + "\n"
+                + str(message.chat.id)
+                + "\n"
+                + url
+                + "\n"
+                + str(e)
+                + "\n"
+                + "\n"
+            )
             file.close()
             bot.reply_to(message, "something went wrong")
-            print(get_current_time() + " id: " + str(id) +
-                ' something went wrong' + '\n' + str(e))
+            print(
+                get_current_time()
+                + " id: "
+                + str(id)
+                + " something went wrong"
+                + "\n"
+                + str(e)
+            )
 
 
 bot.polling(none_stop=True)
