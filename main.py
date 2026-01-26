71d10ac0a0db13d725105667763469f2ba53d5d9
Vadim Levchenko <vadiml@wizardsdev.com>
2026-01-26 21:24:00 +0200

    update User-Agent header

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def get__content(message):
             download_tool_site_2,
             data=data,
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
             },
         )
         try:
